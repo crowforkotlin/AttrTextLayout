2bfed8d04eaa0028e33d0e0e9995ccf60dc07607
WuYa <crowforkotlin@gmail.com>
2023-12-27 11:01:39 +0800

    :label: 解决文本更新策略DEFAULT 无效

--- app/src/main/kotlin/com/crow/attrtextlayout/MainActivity.kt
+++ app/src/main/kotlin/com/crow/attrtextlayout/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     private val mBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val mContent = "静态文本特效/n绘制MS级别/n文本支持多行/n支持多种颜色/n采用了协程框架n采用了协程框架n采用了协程框架"
+
 //    val mContent = "CROW/n自己/n自己/n写的一/n个静态/n文本组/n件，包含了静态文本/n布局 静态文/n本视图，手动计算文本位置 进行对应的绘制！代码量共计1300行左右，十分的简单！算是自定义View中的入门基础了！！/n！/n"
 //    val mContent = "好吧我觉得有BUG-确定吗？？？？我觉得是肯定的！！qweiqx@%!xTIQNAQWENXOQWEM#&IA我阿斯顿维拉4i9992188nnaduqwuzxucqwbdq!@$@#@snajaiw"
 //    val mContent = "好吧我觉得有BUG-确定吗？？？？我觉得是肯定的！！qweiqx@%!xTIQNAQWENXOQWEM#&IA"
