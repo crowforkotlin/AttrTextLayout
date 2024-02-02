c895e8cbbd1252115ef30122412588da664fa92a
WuYa <crowforkotlin@gmail.com>
2024-02-02 15:04:56 +0800

    :fire: 关闭DEBUG

--- crow-attr-text/src/main/kotlin/com/crow/attr/text/IAttrText.kt
+++ crow-attr-text/src/main/kotlin/com/crow/attr/text/IAttrText.kt
@@ -21,7 +21,7 @@ internal interface IAttrText {
          * ● 2023-12-22 15:13:15 周五 下午
          * @author crowforkotlin
          */
-        internal const val DEBUG = true
+        internal const val DEBUG = false
         internal const val DEBUG_TEXT = false
         internal const val DEBUG_ANIMATION = false
 
